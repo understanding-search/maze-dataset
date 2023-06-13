a1cb648022d8e4db35dc86bbca90b27587810a4d
mivanit <mivanits@umich.edu>
2023-06-13 11:37:18 -0600

    added plot_maze and notebooks

--- maze_dataset/utils/notebook_utils.py
+++ maze_dataset/utils/notebook_utils.py
@@ -1,7 +1,17 @@
 import os
+import warnings
 
 import matplotlib.pyplot as plt
-import plotly.io as pio
+
+PLOTLY_IMPORTED: bool
+try:
+    import plotly.io as pio
+except ImportError:
+    warnings.warn("Plotly not installed. Plotly plots will not be available.")
+    PLOTLY_IMPORTED = False
+else:
+    PLOTLY_IMPORTED = True
+
 
 IN_JUPYTER: bool = not "_" in os.environ
 
@@ -30,10 +40,11 @@ def configure_notebook(seed=42, dark_mode=True):
             ipython.magic("autoreload 2")
 
         # Specify plotly renderer for vscode
-        pio.renderers.default = "notebook_connected"
+        if PLOTLY_IMPORTED:
+            pio.renderers.default = "notebook_connected"
 
-        if dark_mode:
-            pio.templates.default = "plotly_dark"
-            plt.style.use("dark_background")
+            if dark_mode:
+                pio.templates.default = "plotly_dark"
+                plt.style.use("dark_background")
 
     return get_device()
