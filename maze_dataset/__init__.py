21f3e29112931d29c03b65404da0d347d5d1e3c4
mivanit <mivanits@umich.edu>
2023-06-14 14:08:09 -0600

    format

--- maze_dataset/__init__.py
+++ maze_dataset/__init__.py
@@ -1,32 +1,25 @@
 from maze_dataset.constants import (
-    Coord, 
-    CoordTup, 
-    CoordList, 
-    CoordArray, 
     SPECIAL_TOKENS,
-)
-from maze_dataset.maze.lattice_maze import (
-    LatticeMaze,
-    SolvedMaze,
-)
-from maze_dataset.dataset.maze_dataset import (
-    MazeDataset, 
-    MazeDatasetConfig,
+    Coord,
+    CoordArray,
+    CoordList,
+    CoordTup,
 )
 from maze_dataset.dataset.collected_dataset import (
     MazeDatasetCollection,
     MazeDatasetCollectionConfig,
 )
-
+from maze_dataset.dataset.maze_dataset import MazeDataset, MazeDatasetConfig
+from maze_dataset.maze.lattice_maze import LatticeMaze, SolvedMaze
 
 __all__ = [
     "Coord",
-	"CoordTup",
-	"CoordList",
-	"CoordArray",
-	"SPECIAL_TOKENS",
-	"LatticeMaze",
-	"SolvedMaze",
+    "CoordTup",
+    "CoordList",
+    "CoordArray",
+    "SPECIAL_TOKENS",
+    "LatticeMaze",
+    "SolvedMaze",
     "MazeDataset",
     "MazeDatasetConfig",
     "MazeDatasetCollection",
