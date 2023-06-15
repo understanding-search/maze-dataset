e74aff836e8869a26c997380849dc79343258001
mivanit <mivanits@umich.edu>
2023-06-14 20:23:18 -0600

    added remove_padding_from_token_str from training evals branch in maze-transformer

--- maze_dataset/tokenization/token_utils.py
+++ maze_dataset/tokenization/token_utils.py
@@ -108,3 +108,8 @@ def coords_to_tokens(
     when_noncoord: WhenMissing = "skip",
 ) -> list[str]:
     return apply_mapping(coords, maze_data_cfg.node_token_map, when_noncoord)
+
+def remove_padding_from_token_str(token_str: str) -> str:
+    token_str = token_str.replace(f"{SPECIAL_TOKENS['padding']} ", "")
+    token_str = token_str.replace(f"{SPECIAL_TOKENS['padding']}", "")
+    return token_str
