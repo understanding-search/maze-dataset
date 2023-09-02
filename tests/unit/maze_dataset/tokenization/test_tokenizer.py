0dbc914649099a911eac71357f4b7f7b6b6c9656
mivanit <mivanits@umich.edu>
2023-09-02 03:25:24 -0600

    tokenizer tests

---
+++ tests/unit/maze_dataset/tokenization/test_tokenizer.py
@@ -0,0 +1,71 @@
+from typing import Iterable
+
+from maze_dataset import SolvedMaze, MazeDataset, MazeDatasetConfig
+from maze_dataset.generation import LatticeMazeGenerators
+
+from maze_dataset.tokenization import MazeTokenizer, TokenizationMode
+from maze_dataset.plotting.print_tokens import  color_maze_tokens_AOTP
+
+
+def test_tokenizer():
+    cfg: MazeDatasetConfig = MazeDatasetConfig(
+        name="test",
+        grid_n=5,
+        n_mazes=3,
+        maze_ctor=LatticeMazeGenerators.gen_dfs,
+    )
+    # to create a dataset, just call MazeDataset.from_config
+    dataset: MazeDataset = MazeDataset.from_config(
+        cfg,
+        do_download=False,
+        load_local=False,
+        do_generate=True,
+        save_local=False,
+        verbose=True,
+        gen_parallel=False,
+    )
+    
+
+    for mode in (TokenizationMode.AOTP_UT_rasterized, TokenizationMode.AOTP_UT_uniform, TokenizationMode.AOTP_indexed):
+        tokenizer: MazeTokenizer = MazeTokenizer(tokenization_mode=mode, max_grid_size=100)
+
+        assert tokenizer.name == f"maze_tokenizer-{mode.name}-g{100}"
+
+        if mode == TokenizationMode.AOTP_indexed:
+            # TODO: fix these asserts
+            # assert tokenizer.node_strings_map is None
+            pass
+        else:
+            # assert tokenizer.node_strings_map is not None
+            # assert len(tokenizer.node_strings_map) == 100
+            assert tokenizer.vocab_size > 100
+        
+        assert isinstance(tokenizer.token_arr, Iterable)
+        assert all(isinstance(token, str) for token in tokenizer.token_arr)
+        assert len(tokenizer.token_arr) == tokenizer.vocab_size
+
+        print(tokenizer.summary())
+
+
+        for maze in dataset:
+            
+            # clear the cache here so we test if it works fine on the next loop
+            tokenizer.clear_cache()
+        
+            maze_tok = maze.as_tokens(maze_tokenizer=tokenizer)
+
+            # you can view the tokens directly
+            print("\nRaw tokens:\n")
+            print(" ".join(maze_tok))
+
+            maze_recovered = SolvedMaze.from_tokens(maze_tok, maze_tokenizer=tokenizer)
+
+            assert (maze.connection_list == maze_recovered.connection_list).all()
+
+            # or color and print them in various formats
+            print("\nColored tokens, raw html:\n")
+            print(color_maze_tokens_AOTP(maze_tok, fmt="html"))
+            print("\nColored tokens, raw latex:\n")
+            print(color_maze_tokens_AOTP(maze_tok, fmt="latex"))
+            print("\nColored tokens, terminal:\n")
+            print(color_maze_tokens_AOTP(maze_tok, fmt="terminal"))
