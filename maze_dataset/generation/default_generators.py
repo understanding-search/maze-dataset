5578ae68ec87b080fa9001d7e153e45617c7819f
mivanit <mivanits@umich.edu>
2023-09-04 01:04:55 -0600

    format

--- maze_dataset/generation/default_generators.py
+++ maze_dataset/generation/default_generators.py
@@ -7,7 +7,10 @@ DEFAULT_GENERATORS: list[tuple[str, dict]] = [
     ("gen_dfs", dict(max_tree_depth=0.5)),
     ("gen_wilson", dict()),
     # ("gen_percolation", dict(p=0.1)),
-    ("gen_percolation", dict(p=1.0)),  # anything less than this and tests will stochastically fail
+    (
+        "gen_percolation",
+        dict(p=1.0),
+    ),  # anything less than this and tests will stochastically fail
     ("gen_dfs_percolation", dict(p=0.1)),
     ("gen_dfs_percolation", dict(p=0.4)),
     # ("gen_prim", dict()),
