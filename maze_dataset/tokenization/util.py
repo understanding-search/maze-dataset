9c43aa5c5b8b39d82bc79eef9cdf364207cdef6b
mivanit <mivanits@umich.edu>
2024-06-10 13:21:20 -0700

    run format

--- maze_dataset/tokenization/util.py
+++ maze_dataset/tokenization/util.py
@@ -266,11 +266,9 @@ def flatten(it: Iterable[any], levels_to_flatten: int | None = None) -> Generato
             and (levels_to_flatten is None or levels_to_flatten > 0)
         ):
             yield from flatten(
-                x, 
-                levels_to_flatten = (
-                    None 
-                    if levels_to_flatten is None 
-                    else levels_to_flatten - 1
+                x,
+                levels_to_flatten=(
+                    None if levels_to_flatten is None else levels_to_flatten - 1
                 ),
             )
         else:
