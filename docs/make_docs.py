502a29b884bb1a98a362d88eca379a3a30179021
mivanit <mivanits@umich.edu>
2024-08-28 18:57:46 -0600

    fix alerts and some relative svg paths

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -12,6 +12,8 @@ import pdoc.render
 import pdoc.render_helpers
 from markupsafe import Markup
 
+pdoc.render_helpers.markdown_extensions["alerts"] = True
+pdoc.render_helpers.markdown_extensions["admonitions"] = True
 
 def increment_markdown_headings(markdown_text: str, increment: int = 2) -> str:
     """
