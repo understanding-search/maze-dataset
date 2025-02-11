6dba7c67777914cd60bd2b873be1981a04774954
mivanit <mivanits@umich.edu>
2025-02-11 12:02:35 -0700

    remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Temporary setup file to install the package locally
-
-from setuptools import setup, find_packages
-
-setup(
-    name="maze_dataset",
-    version="0.1",
-    packages=find_packages(),
-)
