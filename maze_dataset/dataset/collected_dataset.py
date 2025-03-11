3dbfe15c52aea0f4ee96572ab521d9a156ef3e21
Michael Ivanitskiy <mivanits@umich.edu>
2025-03-10 23:06:55 -0600

    no more docstring ruff errors (D1*)

--- maze_dataset/dataset/collected_dataset.py
+++ maze_dataset/dataset/collected_dataset.py
@@ -44,21 +44,26 @@ class MazeDatasetCollectionConfig(GPTDatasetConfig):
 
 	@property
 	def n_mazes(self) -> int:
+		"""return the total number of mazes in the collection across all dataset"""
 		return sum(config.n_mazes for config in self.maze_dataset_configs)
 
 	@property
 	def max_grid_n(self) -> int:
+		"""return the maximum grid size of the mazes in the collection"""
 		return max(config.grid_n for config in self.maze_dataset_configs)
 
 	@property
 	def max_grid_shape(self) -> CoordTup:
+		"""return the maximum grid shape of the mazes in the collection"""
 		return (self.max_grid_n, self.max_grid_n)
 
 	@property
 	def max_grid_shape_np(self) -> Coord:
+		"""return the maximum grid shape of the mazes in the collection as a numpy array"""
 		return np.array(self.max_grid_shape, dtype=np.int32)
 
 	def stable_hash_cfg(self) -> int:
+		"""return a stable hash of the config"""
 		return stable_hash(json.dumps(self.serialize()))
 
 	def to_fname(self) -> str:
@@ -77,6 +82,7 @@ class MazeDatasetCollection(GPTDataset):
 		maze_datasets: list[MazeDataset],
 		generation_metadata_collected: dict | None = None,
 	) -> None:
+		"initialize the dataset collection from a `MazeDatasetCollectionConfig` and a list of `MazeDataset`s"
 		super().__init__()
 		self.cfg: MazeDatasetCollectionConfig = cfg
 		self.maze_datasets: list[MazeDataset] = list(maze_datasets)
@@ -92,14 +98,17 @@ class MazeDatasetCollection(GPTDataset):
 
 	@property
 	def dataset_lengths(self) -> list[int]:
+		"""return the lengths of each dataset in the collection"""
 		return [len(dataset) for dataset in self.maze_datasets]
 
 	@property
 	def dataset_cum_lengths(self) -> Int[np.ndarray, " indices"]:
+		"""return the cumulative lengths of each dataset in the collection"""
 		return np.array(list(itertools.accumulate(self.dataset_lengths)))
 
 	@cached_property
 	def mazes(self) -> list[LatticeMaze]:
+		"single list of all mazes in the collection"
 		return list(
 			itertools.chain.from_iterable(
 				dataset.mazes for dataset in self.maze_datasets
@@ -107,9 +116,11 @@ class MazeDatasetCollection(GPTDataset):
 		)
 
 	def __len__(self) -> int:
+		"""return the total number of mazes in the collection"""
 		return sum(len(dataset) for dataset in self.maze_datasets)
 
 	def __getitem__(self, index: int):
+		"get a maze by index"
 		# find which dataset the index belongs to
 		# we add 1, since np.searchsorted returns the
 		# index of the last element that is strictly less than the target
@@ -128,6 +139,7 @@ class MazeDatasetCollection(GPTDataset):
 		cfg: MazeDatasetCollectionConfig,
 		**kwargs,
 	) -> "MazeDatasetCollection":
+		"""generate a dataset collection from a config"""
 		datasets = [
 			MazeDataset.generate(config, **kwargs)
 			for config in cfg.maze_dataset_configs
@@ -140,6 +152,7 @@ class MazeDatasetCollection(GPTDataset):
 		cfg: MazeDatasetCollectionConfig,
 		**kwargs,
 	) -> "MazeDatasetCollection":
+		"(not implemented!) download a dataset collection from a config"
 		datasets = [
 			MazeDataset.download(config, **kwargs)
 			for config in cfg.maze_dataset_configs
@@ -147,6 +160,7 @@ class MazeDatasetCollection(GPTDataset):
 		return cls(cfg, datasets)
 
 	def serialize(self) -> JSONdict:
+		"""serialize the dataset collection"""
 		return {
 			_FORMAT_KEY: "MazeDatasetCollection",
 			"cfg": self.cfg.serialize(),
@@ -158,6 +172,7 @@ class MazeDatasetCollection(GPTDataset):
 
 	@classmethod
 	def load(cls, data: JSONdict) -> "MazeDatasetCollection":
+		"""load the dataset collection from the representation created by `serialize`"""
 		assert data[_FORMAT_KEY] == "MazeDatasetCollection"
 		return cls(
 			**{
@@ -169,7 +184,8 @@ class MazeDatasetCollection(GPTDataset):
 	# TODO: remove duplication with MazeDatasetConfig().as_tokens() somehow?
 	def as_tokens(
 		self,
-		maze_tokenizer,  # TODO: MazeTokenizer
+		# TODO: MazeTokenizer
+		maze_tokenizer,  # noqa: ANN001
 		limit: int | None = None,
 		join_tokens_individual_maze: bool = False,
 	) -> list[list[str]] | list[str]:
@@ -192,6 +208,7 @@ class MazeDatasetCollection(GPTDataset):
 			return output
 
 	def update_self_config(self) -> None:
+		"update the config to match the number of mazes, and update the underlying configs of each dataset"
 		# TODO: why cant we set this directly? its not frozen, and it seems to work in a regular MazeDataset
 		self.cfg.__dict__["n_mazes"] = len(self)
 		for dataset in self.maze_datasets:
@@ -201,12 +218,12 @@ class MazeDatasetCollection(GPTDataset):
 MazeDatasetCollectionConfig._dataset_class = MazeDatasetCollection
 register_loader_handler(
 	LoaderHandler(
-		check=lambda json_item, path=None, z=None: (
+		check=lambda json_item, path=None, z=None: (  # noqa: ARG005
 			isinstance(json_item, typing.Mapping)
 			and _FORMAT_KEY in json_item
 			and json_item[_FORMAT_KEY].startswith("MazeDatasetCollection")
 		),
-		load=lambda json_item, path=None, z=None: MazeDatasetCollection.load(json_item),
+		load=lambda json_item, path=None, z=None: MazeDatasetCollection.load(json_item),  # noqa: ARG005
 		uid="MazeDatasetCollection",
 		source_pckg="maze_dataset.generation.maze_dataset_collection",
 		desc="MazeDatasetCollection",
