274f89b4f65e3cd40e5b5b2f41d109a6dbd1839e
Michael Ivanitskiy <mivanits@umich.edu>
2025-03-19 12:24:50 -0600

    format

--- maze_dataset/tokenization/modular/fst.py
+++ maze_dataset/tokenization/modular/fst.py
@@ -12,11 +12,11 @@ we cannot circularly import
 import functools
 from typing import ContextManager
 
-from muutils.spinner import SpinnerContext, NoOpContextManager
+import tqdm
 from muutils.misc.numerical import shorten_numerical_to_str
 from muutils.parallel import run_maybe_parallel
+from muutils.spinner import NoOpContextManager, SpinnerContext
 from rust_fst import Set as FstSet
-import tqdm
 
 from maze_dataset.tokenization.modular.all_tokenizers import get_all_tokenizers
 from maze_dataset.tokenization.modular.fst_load import (
@@ -25,12 +25,15 @@ from maze_dataset.tokenization.modular.fst_load import (
 	get_tokenizers_fst,
 )
 
-def _get_tokenizer_name(tokenizer) -> str:
+
+def _get_tokenizer_name(tokenizer) -> str:  # noqa: ANN001
 	return tokenizer.name
 
-def save_all_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) -> FstSet:
-	"""get all the tokenizers, save an fst file at `MMT_FST_PATH` and return the set"""
 
+def save_all_tokenizers_fst(
+	verbose: bool = True, parallel: bool | int = False
+) -> FstSet:
+	"""get all the tokenizers, save an fst file at `MMT_FST_PATH` and return the set"""
 	sp: type[ContextManager] = SpinnerContext if verbose else NoOpContextManager
 
 	with sp(message="getting all tokenizers"):
@@ -44,14 +47,14 @@ def save_all_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) ->
 		parallel=parallel,
 		pbar=tqdm.tqdm,
 		pbar_kwargs=dict(
-			total=n_tokenizers,
-			desc="get name of each tokenizer",
-			disable=not verbose
+			total=n_tokenizers, desc="get name of each tokenizer", disable=not verbose
 		),
 	)
 
 	assert n_tokenizers == len(all_tokenizers_names)
-	print(f"# got {shorten_numerical_to_str(n_tokenizers)} ({n_tokenizers}) tokenizers names")
+	print(
+		f"# got {shorten_numerical_to_str(n_tokenizers)} ({n_tokenizers}) tokenizers names"
+	)
 
 	with sp(message="sorting tokenizer names"):
 		all_tokenizers_names_sorted: list[str] = sorted(all_tokenizers_names)
@@ -71,9 +74,8 @@ def save_all_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) ->
 	return tok_set
 
 
-def check_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) -> FstSet:
+def check_tokenizers_fst(verbose: bool = True, parallel: bool | int = False) -> FstSet:
 	"regen all tokenizers, check they are in the pre-existing fst set"
-
 	sp: type[ContextManager] = SpinnerContext if verbose else NoOpContextManager
 
 	with sp(message="getting all tokenizers from scratch"):
@@ -90,16 +92,18 @@ def check_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) -> Fs
 		parallel=parallel,
 		pbar=tqdm.tqdm,
 		pbar_kwargs=dict(
-			total=n_tokenizers,
-			desc="get name of each tokenizer",
-			disable=not verbose
+			total=n_tokenizers, desc="get name of each tokenizer", disable=not verbose
 		),
 	)
 
 	assert n_tokenizers == len(all_tokenizers_names)
-	print(f"# got {shorten_numerical_to_str(n_tokenizers)} ({n_tokenizers}) tokenizers names")
+	print(
+		f"# got {shorten_numerical_to_str(n_tokenizers)} ({n_tokenizers}) tokenizers names"
+	)
 
-	check_tokenizer_in_fst__do_except = functools.partial(check_tokenizer_in_fst, do_except=True)
+	check_tokenizer_in_fst__do_except = functools.partial(
+		check_tokenizer_in_fst, do_except=True
+	)
 
 	run_maybe_parallel(
 		func=check_tokenizer_in_fst__do_except,
@@ -118,29 +122,32 @@ def check_tokenizers_fst(verbose: bool = True, parallel: bool|int = False) -> Fs
 
 if __name__ == "__main__":
 	import argparse
+
 	arg_parser: argparse.ArgumentParser = argparse.ArgumentParser(
 		description="save the tokenizers fst set"
 	)
 	arg_parser.add_argument(
-		"-c", "--check",
+		"-c",
+		"--check",
 		action="store_true",
 		help="check that all tokenizers are in the pre-existing fst set",
 	)
 	arg_parser.add_argument(
-		"-q", "--quiet",
+		"-q",
+		"--quiet",
 		action="store_true",
-		help="don't show spinners and progress bars"
+		help="don't show spinners and progress bars",
 	)
 	arg_parser.add_argument(
-        "-p",
-        "--parallel",
-        action="store",
-        nargs="?",
-        type=int,
-        const=True,
-        default=False,
-        help="Control parallelization. will run in serial if nothing specified, use all cpus if flag passed without args, or number of cpus if int passed.",
-    )
+		"-p",
+		"--parallel",
+		action="store",
+		nargs="?",
+		type=int,
+		const=True,
+		default=False,
+		help="Control parallelization. will run in serial if nothing specified, use all cpus if flag passed without args, or number of cpus if int passed.",
+	)
 	args: argparse.Namespace = arg_parser.parse_args()
 
 	if args.check:
