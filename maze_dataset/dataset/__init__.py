dd307abe841c8524b17e1d592c250d4d0ebae790
Michael Ivanitskiy <mivanits@umich.edu>
2025-03-22 18:48:11 -0600

    fix some imports

--- maze_dataset/dataset/__init__.py
+++ maze_dataset/dataset/__init__.py
@@ -4,15 +4,19 @@ from maze_dataset.dataset.collected_dataset import (
 	MazeDatasetCollection,
 	MazeDatasetCollectionConfig,
 )
-from maze_dataset.dataset.maze_dataset import MazeDataset, MazeDatasetConfig
+from maze_dataset.dataset.maze_dataset import MazeDataset
+from maze_dataset.dataset.maze_dataset_config import MazeDatasetConfig
 
 __all__ = [
 	# submodules
 	"collected_dataset",
 	"configs",
 	"dataset",
+	"filters",
+	"maze_dataset_config",
 	"maze_dataset",
 	"rasterized",
+	"success_predict_math",
 	# dataset classes
 	"MazeDataset",
 	"MazeDatasetConfig",
