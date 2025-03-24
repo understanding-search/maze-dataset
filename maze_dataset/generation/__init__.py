0a7381d2c9d7a1b02ec092ebcf10aa2d7ceece6a
Michael Ivanitskiy <mivanits@umich.edu>
2025-03-24 16:54:52 -0600

    some docs

--- maze_dataset/generation/__init__.py
+++ maze_dataset/generation/__init__.py
@@ -1,7 +1,6 @@
 """generation functions have signature `(grid_shape: Coord, **kwargs) -> LatticeMaze` and are methods in `LatticeMazeGenerators`
 
 `DEFAULT_GENERATORS` is a list of generator name, generator kwargs pairs used in tests and demos
-
 """
 
 from maze_dataset.generation.generators import (
