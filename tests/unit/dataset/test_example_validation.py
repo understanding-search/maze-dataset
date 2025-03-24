232dc58cc5d4cff2a39b2cff6ae1d4ba7bd5794b
Michael Ivanitskiy <mivanits@umich.edu>
2025-03-23 23:47:13 -0600

    remove intentional fail we added to test

--- tests/unit/dataset/test_example_validation.py
+++ tests/unit/dataset/test_example_validation.py
@@ -1,4 +1,4 @@
-import time
+import sys
 from pathlib import Path
 
 import pytest
@@ -44,12 +44,7 @@ def test_validate_fname(dataset_path_str: str):
 	print(f"{dataset_cfg = }")
 	print(f"{dataset_cfg._stable_str_dump() = }")
 
-	# print python version info
-	import sys
-
 	print(f"{sys.version = }")
-	time.sleep(20)
 
 	assert dataset_fname_path == dataset_fname_loaded
 	assert dataset_fname_path == dataset_fname_new
-	pytest.fail("wip")
